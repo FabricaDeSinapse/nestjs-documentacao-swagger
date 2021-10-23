3cdc2770a6e1b8a3cd3ae4d3f5764b4d70d31e09
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-23 12:41:53 -0300

    docs(users): add `UsersController`

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
